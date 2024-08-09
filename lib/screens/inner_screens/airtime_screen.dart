abcd61b3b7af3a2f3d901fb16d75def50793b10f
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-08-09 13:39:10 +0100

    chore: removed the single cjild crolle view and fixed button spacer position

--- lib/screens/inner_screens/airtime_screen.dart
+++ lib/screens/inner_screens/airtime_screen.dart
@@ -45,77 +45,77 @@ class _AirtimeScreenState extends State<AirtimeScreen> {
           ),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 40),
-              const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Text('Recent',
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
-              ),
-              const SizedBox(height: 14),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: List.generate(
-                      15, (index) => _buildRecentItem('08012345678')),
-                ),
-              ),
-              const SizedBox(height: 30),
-              const Text('Choose An Amount',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 40),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text('Recent',
                   style: TextStyle(color: Colors.white, fontSize: 18)),
-              const SizedBox(height: 30),
-              GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 20,
-                  mainAxisSpacing: 20,
-                  childAspectRatio:
-                      2.5, // Adjust this value to make buttons wider
-                ),
-                itemCount: 6,
-                itemBuilder: (context, index) {
-                  final amounts = [
-                    '₦100',
-                    '₦200',
-                    '₦500',
-                    '₦1000',
-                    '₦2000',
-                    '₦5000'
-                  ];
-                  return _buildAmountButton(amounts[index]);
-                },
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
+            ),
+            const SizedBox(height: 14),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                    15, (index) => _buildRecentItem('08012345678')),
               ),
-
-              const SizedBox(height: 20),
-              _buildAmountInput(),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 8.0),
-                child: Text('Phone Number',
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+            const SizedBox(height: 30),
+            const Text('Choose An Amount',
+                style: TextStyle(color: Colors.white, fontSize: 18)),
+            const SizedBox(height: 30),
+            GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                childAspectRatio:
+                    2.5, // Adjust this value to make buttons wider
               ),
-              const SizedBox(height: 5),
-              _buildPhoneNumberInput(),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 8.0),
-                child: Text('Network',
-                    style: TextStyle(color: Colors.white, fontSize: 18)),
-              ),
-              const SizedBox(height: 5),
-              _buildNetworkDropdown(),
-              const SizedBox(height: 40),
-              CustomButton(text: 'Next', onPressed: () {}),
-              const SizedBox(height: 30),
-            ],
-          ),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                final amounts = [
+                  '₦100',
+                  '₦200',
+                  '₦500',
+                  '₦1000',
+                  '₦2000',
+                  '₦5000'
+                ];
+                return _buildAmountButton(amounts[index]);
+              },
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+            ),
+      
+            const SizedBox(height: 20),
+            _buildAmountInput(),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text('Phone Number',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+            const SizedBox(height: 5),
+            _buildPhoneNumberInput(),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.only(left: 8.0),
+              child: Text('Network',
+                  style: TextStyle(color: Colors.white, fontSize: 18)),
+            ),
+            const SizedBox(height: 5),
+            _buildNetworkDropdown(),
+            const Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 60.0),
+              child: CustomButton(text: 'Next', onPressed: () {}),
+            ),
+          ],
         ),
       ),
     );
